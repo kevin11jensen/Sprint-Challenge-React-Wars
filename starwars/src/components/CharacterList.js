699e62a12f4ce3f63c17ce770d8967f90ff2fc08
Kevin Jensen <jensenk28@yahoo.com>
2020-01-24 10:35:17 -0800

    finished

--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css'
 import styled from 'styled-components';
 
 import CharacterDob from "./CharacterDob";
@@ -12,19 +12,29 @@ import CharacterHair from "./CharacterHair";
 
 
 const List = styled.div`
-
+    display: flex;
+    flex-flow: wrap;
+    justify-content: space-around;
+    align-items: center;
+    border: .5rem solid darkBlue;
+    border-radius: 1rem;
+    margin: 1%;
     color: darkBlue;
 
 `;
 
 const Name = styled.div`
-
-    color: darkBlue;
-
+    font-size: 2rem;
+    background-color: darkBlue;
+    color: #f5f3ce;
+    
+    padding: 1%;
+    border: 2px solid #f5f3ce;
+    border-radius: 1rem;
 `;
 
 const Info = styled.div`
-
+    font-size: 1.6rem;
     color: darkBlue;
 
 `;
@@ -41,14 +51,14 @@ export default function CharacterList(props) {
         <List>
             <Name>{props.people.name}</Name>
             <Info>
-                <CharacterDob birth_year = {props.people.birth_year} />
+                <CharacterDob dob = {props.people.birth_year} />
                 <CharacterSpecies species = {props.people.species} />
                 <CharacterHomeworld homeworld = {props.people.homeworld} />
             </Info>
 
             <Description>
                 <CharacterGender gender = {props.people.gender} />
-                <CharacterEyes eye_color = {props.people.eye_color} />
+                <CharacterEyes eyecolor = {props.people.eye_color} />
                 <CharacterSkin skincolor = {props.people.skin_color} />
                 <CharacterHair haircolor = {props.people.hair_color} />
             </Description>
